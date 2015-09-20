32ecb6061969e05ac8d517d7629281e952d8e76b
Carolineliu <fruitsugar1@outlook.com>
2015-09-20 11:39:35 +0800

    change BasketItem.js constuctor and add a function and change basketItem-spec.js

--- spec/BasketItem-spec.js
+++ spec/BasketItem-spec.js
@@ -13,11 +13,27 @@ describe("BasketItem", function() {
     it("should get correct book and count", function() {
       var basketItem = new BasketItem(book, 2);
 
-      expect(basketItem.getBook().title).toBe("harry-potter-I");
-      expect(basketItem.getBook().price).toBe(8);
+      expect(basketItem._book.title).toBe("harry-potter-I");
+      expect(basketItem._book.price).toBe(8);
       expect(basketItem.count).toBe(2);
 
     });
 
+  });
+
+  describe("function", function() {
+    var book;
+
+    beforeEach(function() {
+      book = new Book("harry-potter-I");
+    });
+
+    it("should get correct book ", function() {
+      var basketItem = new BasketItem(book, 2);
+
+      expect(basketItem.getBook().title).toBe("harry-potter-I");
+      expect(basketItem.getBook().price).toBe(8);
+
+    });
   });
 });
